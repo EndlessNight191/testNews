c4d249cf1cc0a844f0cff2ec63c025b415ad193f
EndlessNigth191 <Pustobaev_v.a@outlook.com>
2022-05-21 13:01:55 +0300

    update login

--- frontend/src/pages/MyAuthorization.vue
+++ frontend/src/pages/MyAuthorization.vue
@@ -1,5 +1,20 @@
 <template>
-  <form class="row g-3 needs-validation" style="display: flex; flex-direction: column; width: 40%; margin: 200px auto;" @submit.prevent="onSubmit">
+
+  <div v-if="model" style="height: 50%; width: 60%; background-color: white; border: 2px black solid; position: absolute; z-index: 1000; margin-left: 20%; margin-top: 10%; border-radius: 15px; padding: 30px">
+    <div>
+        <h5 class="title">Вы успешно зарегестрировались</h5>
+    </div>
+    <div>
+        <p style="color: black">Ваш Логин: {{this.user.username}}</p>
+        <p>Ваш Пароль: {{this.user.password}}</p>
+    </div>
+    <div style="display: flex; flex-direction: column">
+        <button @click="next" type="button" class="btn btn-secondary">Далее, в админ панель</button>
+        <button @click="model = !model" type="button" class="btn btn-secondary" style="margin-top: 10px">Закрыть и остатсья тут</button>
+    </div>
+  </div>
+
+  <form class="row g-3 needs-validation" style="display: flex; flex-direction: column; width: 40%; margin: 200px auto;" @submit.prevent="onSubmit" data-bs-toggle="tooltip" data-bs-placement="top" title="Введите свой логин">
     <div class="col-md-12 position-relative">
       <label for="validationTooltip01" class="form-label" >Name</label>
       <input type="text" class="form-control" id="validationTooltip01" v-model="user.username" required>
@@ -9,7 +24,7 @@
     </div>
     <div class="col-md-12 position-relative">
       <label for="validationTooltip02" class="form-label">Password</label>
-      <input type="text" class="form-control" id="validationTooltip02" v-model="user.password" required>
+      <input type="password" class="form-control" id="validationTooltip02" v-model="user.password" required data-bs-toggle="tooltip" data-bs-placement="top" title="Введите свой пароль">
       <div class="valid-tooltip">
         Looks good!
       </div>
@@ -25,6 +40,7 @@ export default {
   name: 'HelloWorld',
   data(){
     return{
+      model: false,
       user:{
         username: '',
         password: '',
@@ -32,15 +48,20 @@ export default {
     }
   },
   methods: {
+    next(){
+      this.model = false
+      this.$router.push('/MyAdminPanel')
+    },
     onSubmit() {
       axios.post(`http://localhost:5000/auth/adminLogin`,{
         username: this.user.username,
         password: this.user.password,
       })
       .then(response => {
+        console.log(response.data)
         localStorage.setItem('token', response.data.token)
         localStorage.setItem('login', 'true')
-        this.$router.push('/MyAdminPanel')
+        this.model = true
       })
       .catch(e => {
         console.log(e)
